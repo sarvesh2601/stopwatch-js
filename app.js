6e38e4d9b24810832b49e29d6cf6dbc7a54a0fde
sarvesh2601 <84705967+sarvesh2601@users.noreply.github.com>
2022-07-16 03:02:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+
+var min = 0;
+var hour = 0;
+var sec = 0;
+var miliSec = 0;
+var timer;
+let laptime = document.getElementById("Lap-timings");
+let counter = 0;
+let x = [];
+let pastLapTime = {
+    hours:0,
+    minutes:0,
+    seconds:0
+    }
+function callTimer() {
+            
+    sec++;
+    if (sec === 60)  {
+        sec = 0;
+        min++;
+    }
+    if( min === 60){
+        min = 0;
+        hout++;
+    }  
+    document.getElementById("timer").innerHTML = hour + ":" + min + ":" + sec;
+}
+
+
+function Start() {
+    document.getElementById("start").disabled = true;
+    timer = setInterval(callTimer, 1000);
+}
+
+function Stop() {
+    document.getElementById("start").disabled = false;
+    clearInterval(timer);
+}
+
+function Reset() {
+    stop();
+    hour = 0;
+    min = 0;
+    sec = 0;
+    document.getElementById("timer").innerHTML = hour + ":" + min + ":" + sec;
+    laptime.innerHTML = null;
+    pastLapTime.hours = 0;
+    pastLapTime.minutes = 0;
+    pastLapTime.seconds = 0;
+}
+
+
+function lap(){
+    let lapHour = hour - pastLapTime.hours;
+    let lapmin = min - pastLapTime.minutes;
+    let lapsec = sec - pastLapTime.seconds;
+    const currentTime = document.getElementById("timer");
+    const prevLapTime = laptime.innerHTML;
+    laptime.innerHTML =  laptime.innerHTML + lapHour + ":" + lapmin + ":" + lapsec + "<br>" ;
+    pastLapTime.hours = hour;
+    pastLapTime.minutes = min;
+    pastLapTime.seconds = sec;
+}
+
+
